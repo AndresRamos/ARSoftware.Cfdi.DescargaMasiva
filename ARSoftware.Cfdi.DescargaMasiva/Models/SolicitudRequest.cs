d5a42b3e684b520c39925e1c98025dfe5e2f2607
Andres Ramos <andres@arsoft.net>
2022-04-27 14:19:48 -0500

    Se agregan nuevos atributos a la solicitud deacuerdo a los nuevos cambios del SAT

--- ARSoftware.Cfdi.DescargaMasiva/Models/SolicitudRequest.cs
+++ ARSoftware.Cfdi.DescargaMasiva/Models/SolicitudRequest.cs
@@ -1,4 +1,6 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.Linq;
 using ARSoftware.Cfdi.DescargaMasiva.Enumerations;
 
 namespace ARSoftware.Cfdi.DescargaMasiva.Models
@@ -13,19 +15,50 @@ namespace ARSoftware.Cfdi.DescargaMasiva.Models
         public DateTime EndDate { get; private set; }
         public TipoSolicitud RequestType { get; private set; }
         public string SenderRfc { get; private set; }
-        public string[] RecipientRfc { get; private set; }
+        public IEnumerable<string> RecipientsRfcs { get; private set; }
         public string RequestingRfc { get; private set; }
+        public TipoComprobante DocumentType { get; private set; }
+        public EstadoComprobante DocumentStatus { get; private set; }
+        public string ThirdPartyRfc { get; private set; }
+        public string Complement { get; private set; }
+        public string Uuid { get; private set; }
 
-        public static SolicitudRequest CreateInstance(DateTime startDate, DateTime endDete, TipoSolicitud requestType, string senderRfc, string[] recipientRfc, string requestingRfc)
+        public bool HasDocumentType => DocumentType != null;
+        public bool HasDocumentStatus => DocumentStatus != null;
+        public bool HasComplement => !string.IsNullOrWhiteSpace(Complement);
+        public bool HasUuid => !string.IsNullOrWhiteSpace(Uuid);
+
+        public static SolicitudRequest CreateInstance(DateTime startDate,
+                                                      DateTime endDete,
+                                                      TipoSolicitud requestType,
+                                                      string senderRfc,
+                                                      IEnumerable<string> recipientsRfcs,
+                                                      string requestingRfc,
+                                                      TipoComprobante documentType = null,
+                                                      EstadoComprobante documentStatus = null,
+                                                      string thirdPartyRfc = null,
+                                                      string complement = null,
+                                                      string uuid = null)
         {
+            List<string> recipients = recipientsRfcs.ToList();
+            if (recipients.Any() && recipients.Count > 5)
+            {
+                throw new ArgumentOutOfRangeException(nameof(RecipientsRfcs), "There can only be a maximum of 5 recipient RFC.");
+            }
+
             return new SolicitudRequest
             {
                 StartDate = startDate,
                 EndDate = endDete,
                 RequestType = requestType,
                 SenderRfc = senderRfc,
-                RecipientRfc = recipientRfc,
-                RequestingRfc = requestingRfc
+                RecipientsRfcs = recipients,
+                RequestingRfc = requestingRfc,
+                DocumentType = documentType,
+                DocumentStatus = documentStatus,
+                ThirdPartyRfc = thirdPartyRfc,
+                Complement = complement,
+                Uuid = uuid
             };
         }
     }
