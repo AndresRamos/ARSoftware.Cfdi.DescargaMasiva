e7366858ea76b124a64512aec8b7b047b6a12442
Andres Ramos <andres@arsoft.net>
2025-06-19 23:23:56 -0500

    Refactor SOAP action URLs into a new class
    Removed SOAP action URL constants from `CfdiDescargaMasivaWebServiceUrls` and added a new class `DescargaMasivaSoapActionUrls` to better organize the code. This change improves maintainability by separating concerns related to SOAP actions.

--- src/ARSoftware.Cfdi.DescargaMasiva/Constants/CfdiDescargaMasivaWebServiceUrls.cs
+++ src/ARSoftware.Cfdi.DescargaMasiva/Constants/CfdiDescargaMasivaWebServiceUrls.cs
@@ -4,19 +4,10 @@
     {
         public const string AutenticacionUrl = "https://cfdidescargamasivasolicitud.clouda.sat.gob.mx/Autenticacion/Autenticacion.svc";
 
-        public const string AutenticacionSoapActionUrl = "http://DescargaMasivaTerceros.gob.mx/IAutenticacion/Autentica";
-
         public const string SolicitudUrl = "https://cfdidescargamasivasolicitud.clouda.sat.gob.mx/SolicitaDescargaService.svc";
-
-        public const string SolicitudSoapActionUrl = "http://DescargaMasivaTerceros.sat.gob.mx/ISolicitaDescargaService/SolicitaDescarga";
 
         public const string VerificacionUrl = "https://cfdidescargamasivasolicitud.clouda.sat.gob.mx/VerificaSolicitudDescargaService.svc";
 
-        public const string VerificacionSoapActionUrl =
-            "http://DescargaMasivaTerceros.sat.gob.mx/IVerificaSolicitudDescargaService/VerificaSolicitudDescarga";
-
         public const string DescargaUrl = "https://cfdidescargamasiva.clouda.sat.gob.mx/DescargaMasivaService.svc";
-
-        public const string DescargaSoapActionUrl = "http://DescargaMasivaTerceros.sat.gob.mx/IDescargaMasivaTercerosService/Descargar";
     }
 }
---
+++ src/ARSoftware.Cfdi.DescargaMasiva/Constants/DescargaMasivaSoapActionUrls.cs
@@ -0,0 +1,14 @@
+﻿namespace ARSoftware.Cfdi.DescargaMasiva.Constants
+{
+    public static class DescargaMasivaSoapActionUrls
+    {
+        public const string Autentica = "http://DescargaMasivaTerceros.gob.mx/IAutenticacion/Autentica";
+
+        public const string Solicitud = "http://DescargaMasivaTerceros.sat.gob.mx/ISolicitaDescargaService/SolicitaDescarga";
+
+        public const string VerificaSolicitudDescarga =
+            "http://DescargaMasivaTerceros.sat.gob.mx/IVerificaSolicitudDescargaService/VerificaSolicitudDescarga";
+
+        public const string Descargar = "http://DescargaMasivaTerceros.sat.gob.mx/IDescargaMasivaTercerosService/Descargar";
+    }
+}
