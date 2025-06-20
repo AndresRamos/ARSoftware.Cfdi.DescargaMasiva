64bc54bb37acb833b85933b90bd4064c3ae6dd28
Andres Ramos <andres@arsoft.net>
2025-06-19 23:24:05 -0500

    Add RetencionesDescargaMasivaWebServiceUrls class
    Introduces a new static class `RetencionesDescargaMasivaWebServiceUrls` in the `ARSoftware.Cfdi.DescargaMasiva.Constants` namespace. This class includes four constant string fields for URLs related to authentication, request submission, verification, and downloading services for mass download operations.

---
+++ src/ARSoftware.Cfdi.DescargaMasiva/Constants/RetencionesDescargaMasivaWebServiceUrls.cs
@@ -0,0 +1,13 @@
+﻿namespace ARSoftware.Cfdi.DescargaMasiva.Constants
+{
+    public static class RetencionesDescargaMasivaWebServiceUrls
+    {
+        public const string AutenticacionUrl = "https://retendescargamasivasolicitud.clouda.sat.gob.mx/Autenticacion/Autenticacion.svc";
+
+        public const string SolicitudUrl = "https://retendescargamasivasolicitud.clouda.sat.gob.mx/SolicitaDescargaService.svc";
+
+        public const string VerificacionUrl = "https://retendescargamasivasolicitud.clouda.sat.gob.mx/VerificaSolicitudDescargaService.svc";
+
+        public const string DescargaUrl = "https://retendescargamasiva.clouda.sat.gob.mx/DescargaMasivaService.svc";
+    }
+}
