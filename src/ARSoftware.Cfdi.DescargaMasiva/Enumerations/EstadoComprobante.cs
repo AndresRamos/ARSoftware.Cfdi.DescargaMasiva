c4d27db078ddd3f9423736bd8b897e7348f60779
Andres Ramos <andres@arsoft.net>
2025-06-21 14:23:59 -0500

    Actualiza los valores de EstadoComprobante

--- src/ARSoftware.Cfdi.DescargaMasiva/Enumerations/EstadoComprobante.cs
+++ src/ARSoftware.Cfdi.DescargaMasiva/Enumerations/EstadoComprobante.cs
@@ -5,24 +5,29 @@ namespace ARSoftware.Cfdi.DescargaMasiva.Enumerations
     /// <summary>
     ///     Define el estado del comprobante. Valor utilizado en el atributo EstadoComprobante en la peticion de solicitud.
     /// </summary>
-    public sealed class EstadoComprobante : SmartEnum<EstadoComprobante, int>
+    public sealed class EstadoComprobante : SmartEnum<EstadoComprobante, string>
     {
         /// <summary>
         ///     Null
         /// </summary>
-        public static readonly EstadoComprobante Null = new EstadoComprobante("Null", -1);
+        public static readonly EstadoComprobante Null = new("Null", "Null");
 
         /// <summary>
-        ///     Cancelado = 0
+        ///     Todos
         /// </summary>
-        public static readonly EstadoComprobante Cancelado = new EstadoComprobante("Cancelado", 0);
+        public static readonly EstadoComprobante Todos = new("Todos", "Todos");
 
         /// <summary>
-        ///     Vigente = 1
+        ///     Cancelado
         /// </summary>
-        public static readonly EstadoComprobante Vigente = new EstadoComprobante("Vigente", 1);
+        public static readonly EstadoComprobante Cancelado = new("Cancelado", "Cancelado");
 
-        private EstadoComprobante(string name, int value) : base(name, value)
+        /// <summary>
+        ///     Vigente
+        /// </summary>
+        public static readonly EstadoComprobante Vigente = new("Vigente", "Vigente");
+
+        private EstadoComprobante(string name, string value) : base(name, value)
         {
         }
     }
