5eea288a328d39cf0d4852fc5891df51afa326ca
Andres Ramos <andres@arsoft.net>
2025-06-21 22:27:54 -0500

    Refactor GetCertificate method for better key storage
    Updated the `GetCertificate` method in the `X509Certificate2Helper` class to use `X509CertificateLoader.LoadPkcs12` instead of directly instantiating `X509Certificate2`. This change incorporates additional key storage flags (`MachineKeySet`, `PersistKeySet`, and `Exportable`) for improved management of the certificate's key storage.

--- src/ARSoftware.Cfdi.DescargaMasiva/Helpers/X509Certificate2Helper.cs
+++ src/ARSoftware.Cfdi.DescargaMasiva/Helpers/X509Certificate2Helper.cs
@@ -6,8 +6,7 @@ namespace ARSoftware.Cfdi.DescargaMasiva.Helpers
     {
         public static X509Certificate2 GetCertificate(byte[] certificate, string password)
         {
-            return new X509Certificate2(certificate,
-                password,
+            return X509CertificateLoader.LoadPkcs12(certificate, password,
                 X509KeyStorageFlags.MachineKeySet | X509KeyStorageFlags.PersistKeySet | X509KeyStorageFlags.Exportable);
         }
     }
