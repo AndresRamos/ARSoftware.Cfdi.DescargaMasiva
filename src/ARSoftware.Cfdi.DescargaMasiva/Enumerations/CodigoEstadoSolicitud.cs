045a1e8d0d4a45e1c10d3cedf74846977f63252e
Andres Ramos <andres@arsoft.net>
2025-06-22 23:37:20 -0500

    Add codigo 5011 to CodigoEstadoSolicitud

--- src/ARSoftware.Cfdi.DescargaMasiva/Enumerations/CodigoEstadoSolicitud.cs
+++ src/ARSoftware.Cfdi.DescargaMasiva/Enumerations/CodigoEstadoSolicitud.cs
@@ -11,49 +11,40 @@ namespace ARSoftware.Cfdi.DescargaMasiva.Enumerations
         /// <summary>
         ///     5000 = Solicitud recibida con éxito
         /// </summary>
-        public static readonly CodigoEstadoSolicitud _5000 = new CodigoEstadoSolicitud("5000",
-            5000,
-            "Solicitud recibida con éxito",
+        public static readonly CodigoEstadoSolicitud _5000 = new("5000", 5000, "Solicitud recibida con éxito",
             "Indica que la solicitud de descarga que se está verificando fue aceptada.");
 
         /// <summary>
         ///     5002 = Se agotó las solicitudes de por vida
         /// </summary>
-        public static readonly CodigoEstadoSolicitud _5002 = new CodigoEstadoSolicitud("5002",
-            5002,
-            "Se agotó las solicitudes de por vida",
+        public static readonly CodigoEstadoSolicitud _5002 = new("5002", 5002, "Se agotó las solicitudes de por vida",
             "Para el caso de descarga de tipo CFDI, se tiene unlímite máximo para solicitudes con los mismos parámetros(Fecha Inicial, Fecha Final, RfcEmisor, RfcReceptor).");
 
         /// <summary>
         ///     5003 = Tope máximo
         /// </summary>
-        public static readonly CodigoEstadoSolicitud _5003 = new CodigoEstadoSolicitud("5003",
-            5003,
-            "Tope máximo",
+        public static readonly CodigoEstadoSolicitud _5003 = new("5003", 5003, "Tope máximo",
             "Indica que en base a los parámetros de consulta se está superando el tope máximo de CFDI o Metadata, por solicitud de descarga masiva.");
 
         /// <summary>
         ///     5004 = No se encontró la información
         /// </summary>
-        public static readonly CodigoEstadoSolicitud _5004 = new CodigoEstadoSolicitud("5004",
-            5004,
-            "No se encontró la información",
+        public static readonly CodigoEstadoSolicitud _5004 = new("5004", 5004, "No se encontró la información",
             "Indica que la solicitud de descarga que se está verificando no generó paquetes por falta de información.");
 
         /// <summary>
         ///     5005 = Solicitud duplicada
         /// </summary>
-        public static readonly CodigoEstadoSolicitud _5005 = new CodigoEstadoSolicitud("5005",
-            5005,
-            "Solicitud duplicada",
+        public static readonly CodigoEstadoSolicitud _5005 = new("5005", 5005, "Solicitud duplicada",
             "En caso de que exista una solicitud vigente con los mismos parámetros (Fecha Inicial, Fecha Final, RfcEmisor, RfcReceptor, TipoSolicitud), no se permitirá generar una nueva solicitud.");
+
+        public static readonly CodigoEstadoSolicitud _5011 = new("5011", 5011, "Límite de descargas por folio por día",
+            "Se ha alcanzado o sobrepasado el límite de descargas diarias por folio.");
 
         /// <summary>
         ///     404 = Error no Controlado
         /// </summary>
-        public static readonly CodigoEstadoSolicitud _404 = new CodigoEstadoSolicitud("404",
-            404,
-            "Error no Controlado",
+        public static readonly CodigoEstadoSolicitud _404 = new("404", 404, "Error no Controlado",
             "Error genérico, en caso de presentarse realizar nuevamente la petición y si persiste el error levantar un RMA.");
 
         private CodigoEstadoSolicitud(string name, int value, string mensaje, string observaciones) : base(name, value)
